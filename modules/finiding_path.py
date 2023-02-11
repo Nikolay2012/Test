b855e6f8a7f863a143dcded200093bb1706c64d1
Микола Скрипник <nick20122044@gmail.com>
2023-02-11 18:04:39 +0200

    Delete finiding_path.py

--- modules/finiding_path.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def search(file_name):
-    path = __file__.split("/")
-    for i in range(2):
-        del path[-1]
-    path = '/'.join(path)
-    path = os.path.join(path, file_name)
-    return path
